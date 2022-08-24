47ff747a657ff130176ef7a4430229c1ab0fbb4a
Steven Allen <steven@stebalien.com>
2022-08-24 15:50:03 +0200

    feat: use a bitfield in the evm
    This reduces memory usage and may improve caching. On the other hand,
    it's more complex and may be slower. That all depends on the
    cache/instruction trade off.

--- actors/evm/src/interpreter/bytecode.rs
+++ actors/evm/src/interpreter/bytecode.rs
@@ -4,7 +4,7 @@ use {super::opcode::OpCode, crate::interpreter::output::StatusCode, std::ops::De
 
 pub struct Bytecode<'c> {
     code: &'c [u8],
-    jumpdest: Vec<bool>,
+    jumpdest: bitvec::boxed::BitBox,
 }
 
 impl<'c> Bytecode<'c> {
@@ -12,11 +12,11 @@ impl<'c> Bytecode<'c> {
         // only jumps to those addresses are valid. This is a security
         // feature by EVM to disallow jumps to arbitary code addresses.
         // todo: create the jumpdest table only once during initial contract deployment
-        let mut jumpdest = vec![false; bytecode.len()];
+        let mut jumpdest = bitvec::bitbox![0; bytecode.len()];
         let mut i = 0;
         while i < bytecode.len() {
             if bytecode[i] == OpCode::JUMPDEST as u8 {
-                jumpdest[i] = true;
+                jumpdest.set(i, true);
                 i += 1;
             } else if bytecode[i] >= OpCode::PUSH1 as u8 && bytecode[i] <= OpCode::PUSH32 as u8 {
                 i += (bytecode[i] - OpCode::PUSH1 as u8) as usize + 2;
@@ -33,7 +33,7 @@ impl<'c> Bytecode<'c> {
     /// This location must begin with a JUMPDEST opcode that
     /// marks a valid jump destination
     pub fn valid_jump_destination(&self, offset: usize) -> bool {
-        offset < self.jumpdest.len() && self.jumpdest[offset]
+        *self.jumpdest.get(offset).as_deref().unwrap_or(&false)
     }
 }
 
